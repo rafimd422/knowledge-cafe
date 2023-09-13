0e52c80e15ac9b6c5bca51b45ee9c9db02aeef95
rafimd422 <rafimf2222@gmail.com>
2023-09-14 02:44:40 +0600

    added functionality remove from bookmark by another click

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,24 @@ import { useState } from 'react'
 function App() {
 const [bookmark, setBookmark] = useState([])
 
+
 const handleBookmark = (blog) =>{
-const addBookmark = [...bookmark, blog]
-setBookmark(addBookmark)
-console.log(addBookmark)
+  console.log({bookmark})
+
+  let found = false;
+  bookmark.forEach(ele=>{
+    if(ele.id===blog.id) found = true;
+  })
+
+  if(found) {
+    const temp = bookmark.filter(ele=>ele.id !== blog.id)
+    setBookmark(temp)
+  } else {
+    const addBookmark = [...bookmark, blog]
+    setBookmark(addBookmark)
+  }
 }
+
   return (
     <>
  <Header></Header>
--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,14 +1,16 @@
 import Bookmarks from "./Bookmarks";
-const Bookmark = ({setBookmark}) => {
+const Bookmark = ({ setBookmark }) => {
   return (
     <div className="w-64 md:w-1/3 bg-neutral-900 bg-opacity-5 rounded-lg px-2 h-fit my-10">
-          <div className="my-4 text-neutral-900 text-2xl font-semibold">
-      Bookmark: {setBookmark.length}
+      <div className="my-4 text-neutral-900 text-2xl font-semibold">
+        Bookmark: {setBookmark.length}
       </div>
 
-      {setBookmark.map(bookmark => <Bookmarks key={bookmark.id} Bookmark={bookmark}></Bookmarks>)}
+      {setBookmark.map((bookmark) => (
+        <Bookmarks key={bookmark.id} Bookmark={bookmark} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Bookmark;
