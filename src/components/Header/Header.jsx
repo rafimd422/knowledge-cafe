6d0fa6082ec1af9009fa0fceb98df1b667e58d64
rafimd422 <rafimf2222@gmail.com>
2023-09-13 14:21:30 +0600

    added assets and google font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1 className='text-4xl'>Knowledge Cafe</h1>
+      <h1 className='text-4xl font-semibold'>Knowledge Cafe</h1>
     </div>
   )
 }
